ead5165ee65c90317c8b9cb96e431d12daf25dd0
fishpan1209 <fishpan1209@gmail.com>
2016-09-22 01:34:41 -0400

    Update etl.py

--- etl.py
+++ etl.py
@@ -12,7 +12,6 @@ import collections
 def read_csv(filepath):
     
     '''
-    TODO: This function needs to be completed.
     Read the events.csv, mortality_events.csv and event_feature_map.csv files into events, mortality and feature_map.
     
     Return events, mortality and feature_map
@@ -34,24 +33,10 @@ def read_csv(filepath):
 def calculate_index_date(events, mortality, deliverables_path):
     
     '''
-    TODO: This function needs to be completed.
-
-    Refer to instructions in Q3 a
-
-    Suggested steps:
     1. Create list of patients alive ( mortality_events.csv only contains information about patients deceased)
     2. Split events into two groups based on whether the patient is alive or deceased
     3. Calculate index date for each patient
     
-    IMPORTANT:
-    Save indx_date to a csv file in the deliverables folder named as etl_index_dates.csv. 
-    Use the global variable deliverables_path while specifying the filepath. 
-    Each row is of the form patient_id, indx_date.
-    The csv file should have a header 
-    For example if you are using Pandas, you could write: 
-        indx_date.to_csv(deliverables_path + 'etl_index_dates.csv', columns=['patient_id', 'indx_date'], index=False)
-
-    Return indx_date
     '''
     
 
@@ -97,24 +82,11 @@ def calculate_index_date(events, mortality, deliverables_path):
 def filter_events(events, indx_date, deliverables_path):
     
     '''
-    TODO: This function needs to be completed.
-
-    Refer to instructions in Q3 a
-
+    
     Suggested steps:
     1. Join indx_date with events on patient_id
     2. Filter events occuring in the observation window(IndexDate-2000 to IndexDate)
     
-    
-    IMPORTANT:
-    Save filtered_events to a csv file in the deliverables folder named as etl_filtered_events.csv. 
-    Use the global variable deliverables_path while specifying the filepath. 
-    Each row is of the form patient_id, event_id, value.
-    The csv file should have a header 
-    For example if you are using Pandas, you could write: 
-        filtered_events.to_csv(deliverables_path + 'etl_filtered_events.csv', columns=['patient_id', 'event_id', 'value'], index=False)
-
-    Return filtered_events
     '''
     """
     print indx_date
@@ -143,26 +115,13 @@ def filter_events(events, indx_date, deliverables_path):
 def aggregate_events(filtered_events_df, mortality_df,feature_map_df, deliverables_path):
     
     '''
-    TODO: This function needs to be completed.
-
-    Refer to instructions in Q3 a
-
-    Suggested steps:
+    
     1. Replace event_id's with index available in event_feature_map.csv
     2. Remove events with n/a values
     3. Aggregate events using sum to calculate feature value 
     4. Normalize the values obtained above using min-max normalization
     
     
-    IMPORTANT:
-    Save aggregated_events to a csv file in the deliverables folder named as etl_aggregated_events.csv. 
-    Use the global variable deliverables_path while specifying the filepath. 
-    Each row is of the form patient_id, event_id, value.
-    The csv file should have a header .
-    For example if you are using Pandas, you could write: 
-        aggregated_events.to_csv(deliverables_path + 'etl_aggregated_events.csv', columns=['patient_id', 'feature_id', 'feature_value'], index=False)
-
-    Return filtered_events
     '''
     events=pd.merge(filtered_events_df,feature_map_df,how='outer',on='event_id')
     events=events.dropna(subset=['value'])
@@ -207,7 +166,7 @@ def create_features(events, mortality, feature_map):
     aggregated_events = aggregate_events(filtered_events, mortality, feature_map, deliverables_path)
 
     '''
-    TODO: Complete the code below by creating two dictionaries - 
+ 
     1. patient_features :  Key - patient_id and value is array of tuples(feature_id, feature_value)
     2. mortality : Key - patient_id and value is mortality label
     '''
@@ -234,14 +193,14 @@ def create_features(events, mortality, feature_map):
 def save_svmlight(patient_features, mortality, op_file, op_deliverable):
     
     '''
-    TODO: This function needs to be completed
+    
     Create two files:
-    1. op_file - which saves the features in svmlight format. (See instructions in Q3d for detailed explanation)
+    1. op_file - which saves the features in svmlight format.
     2. op_deliverable - which saves the features in following format:
        patient_id1 label feature_id:feature_value feature_id:feature_value feature_id:feature_value ...
        patient_id2 label feature_id:feature_value feature_id:feature_value feature_id:feature_value ...  
     
-    Note: Please make sure the features are ordered in ascending order, and patients are stored in ascending order as well.     
+    the features are ordered in ascending order, and patients are stored in ascending order.     
     '''
     
     
